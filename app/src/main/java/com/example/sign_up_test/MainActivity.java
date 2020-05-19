126f1f63e208d736eb5907157a39d4aba1f78dd9
MASON <aliasmdev@gmail.com>
2020-05-19 19:29:11 +0430

    Complete Hashmap

--- app/src/main/java/com/example/sign_up_test/MainActivity.java
+++ app/src/main/java/com/example/sign_up_test/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     HashMap<String,String> Info_for_signup;
+    String url;
 
     TextView tv;
 
@@ -20,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Info_for_signup.put("firstname","firstname");
+        Info_for_signup.put("lastname","lastname");
+        Info_for_signup.put("socialnumber","socialnumbrt");
+        Info_for_signup.put("phonenumber","phonenumber");
+        Info_for_signup.put("grade","grade");
+        Info_for_signup.put("city","city");
+        Info_for_signup.put("gender","gender");
+        Info_for_signup.put("address","address");
 
         Exchange_of_information exchange_of_information=new Exchange_of_information(Info_for_signup,"");
         exchange_of_information.Send_information();
