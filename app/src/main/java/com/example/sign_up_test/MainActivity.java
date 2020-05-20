9ff7fef91fb1fb3db3cad43745681d5819f34f15
MASON <aliasmdev@gmail.com>
2020-05-20 16:27:23 +0430

    Bardia test kon

--- app/src/main/java/com/example/sign_up_test/MainActivity.java
+++ app/src/main/java/com/example/sign_up_test/MainActivity.java
@@ -6,38 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     HashMap<String,String> Info_for_signup=new HashMap<>();
     String url="https://tabeshma.000webhostapp.com/mysites/showparams.php";
+    ProgressBar pb;
+    EditText fn, ln;
+    String feedback=null;
 
     TextView tv;
+    List<AsyncTask> tasks = new ArrayList<>();
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Info_for_signup.put("a","a");
+        fn=findViewById(R.id.user);
+        ln=findViewById(R.id.password);
 
-        final MyHttpUtils.RequestData requestData =
-                new MyHttpUtils.RequestData(url, "POST");
-
-
-        for (Map.Entry<String, String> entry : Info_for_signup.entrySet()) {
-            requestData.setParameter(entry.getKey(),entry.getValue());
-        }
-        new MyTask().execute(requestData);
-
-        tv=findViewById(R.id.textView);
-
-
+        pb=findViewById(R.id.progressBar);
+        pb.setVisibility(View.INVISIBLE);
+        tv=findViewById(R.id.show);
 
 
 
@@ -48,8 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
-
+            if(tasks.isEmpty()){
+                pb.setVisibility(View.VISIBLE);
+            }
+            tasks.add(this);
         }
+
+
 
         @Override
         protected String doInBackground(MyHttpUtils.RequestData... params) {
@@ -65,10 +71,42 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            tv.setText(result);
+          tv.setText(result);
+            tasks.remove(this);
+            if(tasks.isEmpty()){
+                pb.setVisibility(View.INVISIBLE);
+            }
 
         }
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuItem=menu.add("SendData");
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                final MyHttpUtils.RequestData requestData =
+                        new MyHttpUtils.RequestData(url, "POST");
+
+                Info_for_signup.put("firstname",fn.getText().toString().trim());
+                Info_for_signup.put("lastname",ln.getText().toString().trim());
+
+                for (Map.Entry<String, String> entry : Info_for_signup.entrySet()) {
+                    requestData.setParameter(entry.getKey(),entry.getValue());
+                }
+
+                new MyTask().execute(requestData);
+
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
 }
+
+
+
